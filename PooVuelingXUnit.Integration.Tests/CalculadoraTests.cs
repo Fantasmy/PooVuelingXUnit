66f9717e447789fbf23c24c618ba9fa0e27c46c4
Jenny <fantazmit@gmail.com>
2018-06-22 00:07:54 +0200

    new project Xunit testing

---
+++ PooVuelingXUnit.Integration.Tests/CalculadoraTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVuelingXUnit.Integration.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+
+    }
+}
