c7aee49aeb9ae7bca02c675316e81f2947813146
Jenny <fantazmit@gmail.com>
2018-06-24 20:38:36 +0200

    Integration tests

--- PooVuelingXUnit.Integration.Tests/CalculadoraTests.cs
+++ PooVuelingXUnit.Integration.Tests/CalculadoraTests.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using Xunit;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PooVuelingXUnit.Integration.Tests
+namespace poVueling.XUnit.Tests
 {
     public class CalculadoraTests
     {
-        [Fact]
+        ICalculadora iCalculadora = new Calculadora();
+
+        [Theory]
+        [InlineData(4, 2, 2)]
+        [InlineData(9, 3, 3)]
+        public void DivisionTest(int num1, int num2, int result)
+        {
+            Assert.True(iCalculadora.Division(num1, num2) == result);
+        }
+
 
     }
 }
---
+++ PooVuelingXUnit.Unit.Tests/CalculadoraTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poVueling.XUnit.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(2, Divide(4, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(3, Divide(4, 2));
+        }
+
+        int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
